ca540840c3cfc2b91c31a4f3438fad66bebd752b
kbakaras <kbakaras@gmail.com>
2018-12-20 21:34:27 +0300

    Методы для use-флагов в обёртке E2Table.

--- src/main/java/ru/kbakaras/e2/message/E2Table.java
+++ src/main/java/ru/kbakaras/e2/message/E2Table.java
@@ -19,6 +19,19 @@ public class E2Table implements Iterable<E2Row> {
         return xml.attributeValue(E2.TABLE_NAME);
     }
 
+    public E2AttributeUse use() {
+        String use = xml.attributeValue(E2.USE);
+
+        if (use == null || E2.USE_Always.equals(use)) {
+            return E2AttributeUse.Always;
+        } else if (E2.USE_Create.equals(use)) {
+            return E2AttributeUse.Create;
+
+        } else {
+            throw new E2Exception4Read("Invalid value for attribute's flag `use`: " + use);
+        }
+    }
+
     public E2Row rowOrNull(int index) {
         List<Element> rowElements = xml.elements(E2.ROW);
         return index < rowElements.size() ? new E2Row(rowElements.get(index)) : null;
@@ -48,6 +61,17 @@ public class E2Table implements Iterable<E2Row> {
 
     public E2Table setTableName(String name) {
         this.xml.addAttribute(E2.TABLE_NAME, name);
+        return this;
+    }
+
+    public E2Table setUse(E2AttributeUse use) {
+        String useValue = null;
+
+        if (use == E2AttributeUse.Create) {
+            useValue = E2.USE_Create;
+        }
+
+        xml.addAttribute(E2.USE, useValue);
         return this;
     }
 
